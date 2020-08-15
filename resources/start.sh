06ca7ef5d12f5c9eb578accb814a9cec060f31eb
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2020-08-14 21:04:12 -0400

    Change the time and the output from the start.sh script.

--- resources/start.sh
+++ resources/start.sh
@@ -10,24 +10,24 @@ CYAN="\033[36m"
 WHITE="\033[37m"
 NORMAL="\033[0;39m"
 
-# while IFS= read -r line; do
-#     printf $RED
-#     echo "$line" | pv -qL $[25+(-2 + RANDOM%5)]
-# done < title-render.txt
+while IFS= read -r line; do
+    printf $RED
+    echo -e "$line" | pv -qL $[18+(-2 + RANDOM%5)]
+done < title-render.txt
 
 printf $NORMAL
 
 printf $BLUE
 
 echo ""
-echo -e "Whats\b's up everybody? This is Greg!" | pv -qL $[10+(-2 + RANDOM%5)]
+echo -e "Whats\b's up everybody? This is Greg!" | pv -qL $[13+(-2 + RANDOM%5)]
 echo ""
 
-echo -e "I'm coming to you lo\bive from a l\bLinux terminal window!" | pv -qL $[11+(-2 + RANDOM%5)]
+echo -e "I'm coming to you lo\bive 😉 from a l\bLinux terminal window!" | pv -qL $[13+(-2 + RANDOM%5)]
 echo ""
 
-echo -e "Wait ....\b I guess that this is not \e[3mreally\e[0m ${BLUE}live!" | pv -qL $[10+(-2 + RANDOM%5)]
-echo ""
+# echo -e "Wait ....\b I guess that this is not \e[3mreally\e[0m ${BLUE}live!" | pv -qL $[10+(-2 + RANDOM%5)]
+# echo ""
 
 printf $NORMAL
 
