2a8400eaf9348f8edc3d3bb50c32a535fffaeff3
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2020-08-14 20:44:54 -0400

    Add the start.sh script with the introductory content.

---
+++ resources/start.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+BLACK="\033[30m"
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+BLUE="\033[34m"
+PINK="\033[35m"
+CYAN="\033[36m"
+WHITE="\033[37m"
+NORMAL="\033[0;39m"
+
+# while IFS= read -r line; do
+#     printf $RED
+#     echo "$line" | pv -qL $[25+(-2 + RANDOM%5)]
+# done < title-render.txt
+
+printf $NORMAL
+
+printf $BLUE
+
+echo ""
+echo -e "Whats\b's up everybody? This is Greg!" | pv -qL $[10+(-2 + RANDOM%5)]
+echo ""
+
+echo -e "I'm coming to you lo\bive from a l\bLinux terminal window!" | pv -qL $[11+(-2 + RANDOM%5)]
+echo ""
+
+echo -e "Wait ....\b I guess that this is not \e[3mreally\e[0m ${BLUE}live!" | pv -qL $[10+(-2 + RANDOM%5)]
+echo ""
+
+printf $NORMAL
+
+printf $RED
+echo "But, ..." | pv -qL $[10+(-2 + RANDOM%5)]
+echo "LET'S GET STARTED! 🚀"
+echo ""
+
+printf $NORMAL
